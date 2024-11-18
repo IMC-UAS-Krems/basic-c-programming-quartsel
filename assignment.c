67f3b917989f15b2d8e060a2e0d3069dd17fef9f
Lena Handschuh <137884829+quartsel@users.noreply.github.com>
2024-11-18 20:29:34 +0100

    first try

--- assignment.c
+++ assignment.c
@@ -12,7 +12,61 @@ int main(int argc, char *argv[]) {
     int maxrand = 100;
 
     // WRITE YOUR CODE HERE
+    // check for two arguments, return error if incorrect
+    if (argc != 3) {
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2", argc-1);
+        return 0;
+    }
+
+    // convert arguments to int
+    int row = atoi(argv[1]);
+    int col = atoi(argv[2]);
+
+
+    // check if they are positive
+    if (row < 0 || col < 0) {
+        printf("Incorrect usage. The parameters you provided are not positive integers");
+        return 1;
+    }
+
+    // create and open file, show error if it didnt work
+    FILE *fptr;
+    fptr = fopen("matrix.txt", "w");
+    if (fptr == NULL) {
+        printf("Error opening file");
+    }
+
+    //  create dynamically allocated matrix array
+    int **matrix = (int **)malloc(row * sizeof(int *));
+    for (int i = 0; i < row; i++) { 
+        matrix[i] = (int *)malloc(col * sizeof(int));
+        for (int j = 0; j < col; j++) {
+            matrix[i][j] = rand() % (maxrand - minrand + 1) + minrand;
+        }
+    }
     
+
+
+    // add matrix into file with whitespaces, new lines and carriage return and close
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            fprintf(fptr, "%d ", matrix[i][j]);
+            if (j < col - 1) {
+                fprintf(fptr, " ");
+            }
+        }
+        if (i != row - 1){
+            fprintf(fptr, "\n");
+        }
+
+        fprintf(fptr, "\r");
+    }
+    fclose(fptr);
+
+    for (int i = 0; i < row; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
 
     return 0;
 }
